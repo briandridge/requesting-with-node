0d6678eb7661d75917734343b7363db878bb1c2c
briandridge <briandridge@gmail.com>
2016-12-12 14:35:35 -0700

    works

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var threeFavouriteMovies = ["The Fall", "Frozen", "Frozen"];
 threeFavouriteMovies.forEach(function(film) {
 	console.log("from app.js");
 	console.log(film);
-	getMovie(film);
+	movie(film);
 
 });
